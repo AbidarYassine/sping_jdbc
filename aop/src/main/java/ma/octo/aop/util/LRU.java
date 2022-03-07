97b74e8d3705f14bd0e7a78e8ede95a94eb287fa
AbidarYassine <yassine.abidar@edu.uca.ma>
2022-03-07 10:40:11 +0100

    add spring jdbc and h2 db

---
+++ aop/src/main/java/ma/octo/aop/util/LRU.java
@@ -0,0 +1,10 @@
+package ma.octo.aop.util;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface LRU {
+    String value() default "";
+}
